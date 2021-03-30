ef67a8c635c88ecc8f9848482d14b937cc2d3a90
kelschbledsoe <kelschbl@msu.edu>
2021-03-29 20:59:21 -0400

    Make additional mort req functional

--- src/AdditionalRequest/AdditionalRequest.js
+++ src/AdditionalRequest/AdditionalRequest.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useReducer, useState } from 'react';
+import { API, graphqlOperation } from 'aws-amplify';
+import * as mutations from './../graphql/mutations';
+import { listCompanys } from './../graphql/queries'
 
 // reactstrap components
 import {
@@ -15,8 +18,74 @@ import {
     Label
   } from "reactstrap";
 
-function AdditionalRequest() {
+  const formReducer = (state, event) => {
+    if(event.reset) {
+      return {
+        ClientId: '',
+        AgentId: '',
+        Company: '',
+      }
+    }
+    return {
+      ...state,
+      [event.name]: event.value
+    }
+  }
 
+function AdditionalRequest() {
+  const [companys, setCompanys] = useState([])
+  let listofcompanys;
+  async function queryCompany(){
+    /* make page take longer to load await new Promise(x=>setTimeout(x,10000)) */
+    const models = await API.graphql(graphqlOperation(listCompanys))
+    listofcompanys = models.data.listCompanys.items
+    if(listofcompanys){
+      setCompanys(listofcompanys);
+    }
+  }
+  queryCompany()
+
+  const [formData, setFormData] = useReducer(formReducer, {});
+  // This event is the Submit button behavior. Has a cool down period to let the API catch up then has a JS alert box.
+  const [submitting, setSubmitting] = useState(false);
+  const handleSubmit = event => {
+    event.preventDefault();
+    setSubmitting(true);
+    setTimeout(() => {
+      setSubmitting(false);
+      setFormData({
+        reset: true
+      })
+    }, 3000)
+  }
+
+  const handleChange = event => {
+    setFormData({
+      name: event.target.name,
+      value: event.target.value,
+    });
+  }
+
+  async function makeRequest(){
+    let companyId;
+    companys.map(function(company){
+      if(!company.companyId){
+        return;
+      }
+      if(String(formData.Company) === String(company.name)){
+      companyId = company.companyId;
+      }
+    })
+    const createMortgageRequest =
+      {
+        status: 1,
+        clientId: formData.ClientId,
+        agentId: formData.AgentId,
+        companyId: companyId,
+      }
+      const newMortgageRequest = await API.graphql({ query: mutations.createMortgageRequest, variables:{input: createMortgageRequest}});
+
+  }
 
     return (
       <>
@@ -30,36 +99,39 @@ function AdditionalRequest() {
                 <CardHeader>Please complete all fields to submit your request.</CardHeader>
                 <CardHeader>Enter your ID and the ID of the client you would like to make additional mortgage request for, then select the company you would like to send the request to.</CardHeader>
                 <CardBody>
-                  <Form>
+                  <Form onSubmit={handleSubmit}>
                     <Row>
                       <Col className="pr-md-1" md="6">
                         <FormGroup>
                           <label>Client ID</label>
-                          <Input
-                            placeholder="ID"
-                            type="text"
+                          <Input type="text" name="ClientId" id="ClientId" placeholder="Client ID" 
+                          onChange={handleChange}
+                          value={formData.ClientId || ''}
                           />
                         </FormGroup>
                         <FormGroup>
                           <label>Agent ID</label>
-                          <Input
-                            placeholder="ID"
-                            type="text"
+                          <Input type="text" name="AgentId" id="AgentId" placeholder="Agent ID" 
+                          onChange={handleChange}
+                          value={formData.AgentId || ''}
                           />
                         </FormGroup>
                       </Col>
-                      
                     </Row>
                     <FormGroup row>
                     <Label for="SelectCompany" sm={2}>Select one company to send a mortgage request to.</Label>
                     <Col sm={10}>
-                      <Input type="select" name="Company" id="SelectCompany">
+                    <Input type="select" name="Company" id="SelectCompany"
+                      onChange={handleChange}
+                      value={formData.Company || ''}>
                         <option></option>
-                        <option>Chase</option>
-                        <option>PNC Bank</option>
-                        <option>Quicken Loans</option>
-                        <option>Rocket Mortgage</option>
-                        <option>USAA</option>
+                        {
+                        companys.map(function(company)
+                        {
+                          if(!company.companyId){
+                            return;
+                          }
+                          return(<option>{company.name}</option>)})}
                       </Input>
                     </Col>
                   </FormGroup>
@@ -67,7 +139,7 @@ function AdditionalRequest() {
                 </CardBody>
                 <CardFooter>
                   <Button color="warning" href='/'>Back</Button>{' '}
-                  <Button onClick={()=>alert('Your request has been submitted')} className="btn-fill" color="warning" type="submit" href='/'>
+                  <Button onClick={()=>makeRequest()} className="btn-fill" color="warning" type="submit" href='/'>
                     Submit
                   </Button>
                 </CardFooter>
