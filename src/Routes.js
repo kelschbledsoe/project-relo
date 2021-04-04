a997de29aac82a5bbb5deba303e7cf6d38d30a05
Jihwan Kim <jihwan2164@gmail.com>
2021-04-04 14:38:24 -0400

    making conditions to split view

--- src/Routes.js
+++ src/Routes.js
@@ -18,27 +18,53 @@ import EditAgent from './EditAgent/EditAgent'
 import EditCompany from './EditCompany/EditCompany'
 
 export default class Routes extends Component {
-    render() {
-        return (
-            <Router history={history}>
-                <Switch>
-                    <Route path="/" exact component={Home} />
-                    <Route path="/NewRequest" component={NewRequest} />
-                    <Route path="/ClientList" component={ClientList} />
-                    <Route path="/AddAgent" component={AddAgent} />
-                    <Route path="/AddCompany" component={AddCompany}/>
-                    <Route path="/ClientDetail" component={ClientDetail}/>
-                    <Route path="/CompanyDetail" component={CompanyDetail}/>
-                    <Route path="/Search" component={Search}/>
-                    <Route path="/AgentDetail" component={AgentDetail}/>
-                    <Route path="/AdminCompanyDetail" component={AdminCompanyDetail}/>
-                    <Route path="/Home-Agent" component={AgentHome}/>
-                    <Route path="/AdditionalRequest" component={AdditionalRequest}/>
-                    <Route path="/EditAgent" component={EditAgent} />
-                    <Route path="/EditCompany" component={EditCompany} />
 
-                </Switch>
-            </Router>
-        )
+    render() {
+
+        const url = window.location.href;
+        if (url.includes("agent")){
+            return(
+                <Router history={history}>
+                    <Switch>
+                        <Route path="/" exact component={AgentHome} />
+                        <Route path="/NewRequest" component={NewRequest} />
+                        <Route path="/ClientList" component={ClientList} />
+                        <Route path="/AddAgent" component={AddAgent} />
+                        <Route path="/AddCompany" component={AddCompany}/>
+                        <Route path="/ClientDetail" component={ClientDetail}/>
+                        <Route path="/CompanyDetail" component={CompanyDetail}/>
+                        <Route path="/Search" component={Search}/>
+                        <Route path="/AgentDetail" component={AgentDetail}/>
+                        <Route path="/AdminCompanyDetail" component={AdminCompanyDetail}/>
+                        <Route path="/AdditionalRequest" component={AdditionalRequest}/>
+                        <Route path="/EditAgent" component={EditAgent} />
+                        <Route path="/EditCompany" component={EditCompany} />
+
+                    </Switch>
+                </Router>
+            )
+        }
+        else{
+            return (
+                <Router history={history}>
+                    <Switch>
+                        <Route path="/" exact component={Home} />
+                        <Route path="/NewRequest" component={NewRequest} />
+                        <Route path="/ClientList" component={ClientList} />
+                        <Route path="/AddAgent" component={AddAgent} />
+                        <Route path="/AddCompany" component={AddCompany}/>
+                        <Route path="/ClientDetail" component={ClientDetail}/>
+                        <Route path="/CompanyDetail" component={CompanyDetail}/>
+                        <Route path="/Search" component={Search}/>
+                        <Route path="/AgentDetail" component={AgentDetail}/>
+                        <Route path="/AdminCompanyDetail" component={AdminCompanyDetail}/>
+                        <Route path="/AdditionalRequest" component={AdditionalRequest}/>
+                        <Route path="/EditAgent" component={EditAgent} />
+                        <Route path="/EditCompany" component={EditCompany} />
+
+                    </Switch>
+                </Router>
+            )
+        }
     }
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,6 +7,9 @@ import { AmplifySignOut} from '@aws-amplify/ui-react'
 
 const Navigation = (props) => {
     console.log(props);
+    const url = window.location.href;
+    if (url.includes("agent"))
+    {
     return (
         <Navbar className="color-nav" variant="dark">
             <Navbar.Brand href="/" >Project Relo</Navbar.Brand>
@@ -14,25 +17,164 @@ const Navigation = (props) => {
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
                     <Nav.Link href="/">Home</Nav.Link>
-                    <Nav.Link href="/">Admin:</Nav.Link>
-                    <Nav.Link href="/AddAgent">Add Agent</Nav.Link>
-                    <Nav.Link href="/AddCompany">Add Company</Nav.Link>
-                    <Nav.Link href="/Search">Search</Nav.Link>
-                    <Nav.Link href='/EditAgent'>Edit Agent</Nav.Link>
-                    <Nav.Link href='/EditCompany'>Edit Company</Nav.Link>
-                    <Nav.Link href="/">{""}</Nav.Link>
-                    <Nav.Link href="/">{""}</Nav.Link>
-                    <Nav.Link href="/">{""}</Nav.Link>
-
-                    <Nav.Link href="/Home-Agent">Agent:</Nav.Link>
                     <Nav.Link href="/NewRequest">New Request</Nav.Link>
                     <Nav.Link href="/ClientList">Client List</Nav.Link>
                     <Nav.Link href='/AdditionalRequest'>Create Additional Mortgage Request</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
                     <AmplifySignOut></AmplifySignOut>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
     )
+    }
+    else{
+        return(
+            <Navbar className="color-nav" variant="dark">
+            <Navbar.Brand href="/" >Project Relo</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/AddAgent">Add Agent</Nav.Link>
+                    <Nav.Link href="/AddCompany">Add Company</Nav.Link>
+                    <Nav.Link href="/Search">Search</Nav.Link>
+                    <Nav.Link href='/EditAgent'>Edit Agent</Nav.Link>
+                    <Nav.Link href='/EditCompany'>Edit Company</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <Nav.Link>{' '}</Nav.Link>
+                    <AmplifySignOut></AmplifySignOut>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+        )
+    }
 }
 
 export default withRouter(Navigation);
